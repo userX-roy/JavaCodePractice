5759dd1562ac7c2bd3a965bceefd8266a4209894
saikatr <saikatroyxyz@gmail.com>
2022-08-21 22:03:15 +0530

    Final Commit

--- JavaSource/src/nareshit/assigenment/com/OtherClasses.java
+++ JavaSource/src/nareshit/assigenment/com/OtherClasses.java
@@ -32,23 +32,26 @@ class OtherClasses {
 	}
 	
 	void eachProductCount() {
-		System.out.println("++++++++++Your Product Summary++++++++++");
+		System.out.println("++++++++++Your Each Product Summary++++++++++");
 		System.out.println("You buy Fruit amount of Rs. "+Fruit.priceF);
 		System.out.println("You buy Vegitable amount of Rs. "+Vegetables.priceV);
 		System.out.println("You buy Cosmatics amount of Rs. "+Cosmetics.priceC);
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++");
 	}
 	
+	//will take address from user
 	void addressInput() {
 		System.out.println("Please Provide your address , so that we can ship the peoducts");
 		addrString = scanner2.nextLine();
 	}
 	
+	//will show address from user
 	void addressShow() {
 		System.out.println("We will ship the Product to the following address :");
 		System.out.println(addrString);
 	}
 	
+	//will calculate if eligible for discount or not
 	float discountCalculator(float totalPrice) {
 		float amountAfterDiscount = totalPrice;
 		
@@ -62,11 +65,14 @@ class OtherClasses {
 		}
 	}
 	
+	// responsible for printing the buying summaary
 	void afterCompletionBuy() {
 		addressInput(); // will input customer address
 		
 		System.out.println("=======================================================");
 		
+		System.out.println("-------------Your Buying Summary------------------------");
+		System.out.println(" ");
 		eachProductCount(); // will list down each types of products price individually
 		System.out.println("Your Total Amount is : "+Main.totalPrice);
 		if (Main.totalPrice == 500.0f) {
