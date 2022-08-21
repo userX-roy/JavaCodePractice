730b2eacc101bdbe8d1ad06584636d72b05f9811
saikatr <saikatroyxyz@gmail.com>
2022-08-21 19:53:32 +0530

    Dummy commit , bug is still there

--- JavaSource/src/nareshit/assigenment/com/OtherClasses.java
+++ JavaSource/src/nareshit/assigenment/com/OtherClasses.java
@@ -3,16 +3,18 @@ package nareshit.assigenment.com;
 import java.util.Scanner;
 
 public class OtherClasses {
+	
 	// this function will decide if the program terminate or not
 	void continuationConformer(){
 		Scanner scanner2 = new Scanner(System.in);
 		System.out.println("Do you want to continue ? yes or no ?");
-		//Scanner scanner2 = new Scanner(System.in);
 		String inputString = scanner2.next();
-		scanner2.close();
+		
 		if (inputString=="yes"||inputString=="Yes"|| inputString =="YES"||inputString =="Y") {
+			scanner2.close();
 			new Main().mainListofProduct();
 		}
+		
 		else {
 			scanner2.close();
 			return;
