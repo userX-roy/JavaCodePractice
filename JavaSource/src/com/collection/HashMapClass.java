6934a3885e7a640438dfb7c275bea2039af14c79
saikat <saikatroyxyz@gmail.com>
2022-10-10 08:43:56 +0530

    one more collection class added

---
+++ JavaSource/src/com/collection/HashMapClass.java
@@ -0,0 +1,18 @@
+package com.collection;
+
+import java.util.HashMap;
+
+public class HashMapClass {
+
+	void meth1() {
+		HashMap<Object, Object> map = new HashMap<>();
+		
+		map.put(1, "Saikat");
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
